df2b2100bcc3eec0175ce2ecb3fc358c881b72aa
morrow-sys <baktiyar.beisheev@kstu.kg>
2025-09-07 21:10:12 +0600

    Update server.js

--- server.js
+++ server.js
@@ -816,6 +816,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+   ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 // ===== Multer =====
